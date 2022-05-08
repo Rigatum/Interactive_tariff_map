feac54c8696079bf1de1e437b4061a823a437210
Tagir <tagir-csgo@mail.ru>
2022-05-08 11:36:14 +0700

    Initial commit

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+
+$pqr1_ip = "127.0.0.1";
+$par2_name = "root";
+$par3_p = "";
+$par4_db = "tariffs";
+
+$induction = mysqli_connect($pqr1_ip, $par2_name, $par3_p, $par4_db);
+
+if ($induction == false)
+{
+    echo "Ошибка подключения";
+}
+
+?>
